62ae8c19aba1c39beb3c1d08f36e3df9edd4b338
Mitanshu Agrawal <mishu10005agrawal@gmail.com>
2024-07-31 22:15:32 +0530

    Your descriptive commit message

--- src/login_page/first.jsx
+++ src/login_page/first.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -24,7 +24,8 @@ const FirstPage = () => {
   ];
 
   return (
-    <div className='h-auto w-screen flex flex-col justify-center gap-8 items-center bg-violet-200'>
+    <div className='h-auto w-screen flex flex-col justify-center gap-8 items-center bg-orange-100 text-gray-800'>
+      <div className='p-3 bg-orange-400 w-screen justify-center items-center '>RESUME</div>
       <div className="w-full tablet:w-4/5 sm:w-3/4">
         <Slider {...settings} className='w-full'>
           {slides.map((slide, index) => (
@@ -37,18 +38,32 @@ const FirstPage = () => {
           ))}
         </Slider>
       </div>
-      <div className='bg-violet-500 h-4/5 w-full flex flex-col items-center justify-center rounded-lg tablet:w-3/4'>
-        <Link to="/login" className='flex justify-center items-center w-full'>
-          <img src='../public/images/undraw.png' alt='logo' className='w-1/2 h-auto rounded-full m-10 ' />
-        </Link>
-        <h1 className='flex flex-col justify-center items-center rounded-lg m-5 font-get text-3xl'>
-          Craft your personalized resume effortlessly on this platform
-        </h1>
-        <Link to="/login" className='m-10 bg-violet-300 h-auto p-5 rounded-lg w-1/2 flex flex-col items-center font-get text-2xl sm:w-'>
-          Get Start
-        </Link>
-      </div>
+      <h2 className='text-2xl font-bold text-gray-800 mt-4'>Pick Template</h2>
+
+      <img
+        src='../public/images/temp1.png'
+        alt='Pick Template'
+        className='w-3/4 h-auto rounded-lg shadow-md '
+      />
+
+      <h2 className='text-2xl font-bold text-gray-800 mb-4'>Customize the Template</h2>
+
+
+      <img
+        src='../public/images/temp3.png'
+        alt='Customize Template'
+        className='w-3/4 h-auto rounded-lg shadow-md'
+      />
+
+      <h2 className='text-2xl font-bold text-gray-800 mb-4'>Hits and Download</h2>
+      <img
+        src='../public/images/temp2.png'
+        alt='Hits and Download'
+        className='w-full h-auto rounded-lg shadow-md'
+      />
+
     </div>
+
   );
 };
 
